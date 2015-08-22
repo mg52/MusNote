c45cd655551076ad1607f1c0226c48aaedd161eb
Mustafa <mgords@gmail.com>
2015-08-22 14:30:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var app = express();
 var server = app.listen(port);
 
 var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use( bodyParser.json() );      
+app.use(bodyParser.urlencoded({    
   extended: true
 })); 
 
