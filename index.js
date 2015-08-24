c8d435eb107bc225838981e352f3fd5deb959a34
Mustafa <mgords@gmail.com>
2015-08-24 11:48:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ var deleteduser = '';
 var sendusers = new String;
 
 app.get('/', function (req, res) {
-	req.session.reset();
+	if(req.session)	req.session.reset();
 	if(req.session.user){
 	if(!req.session.admin)
 		res.redirect('/login');
