16730c4f04f198c525f332fab1eb79bc2e51876d
Mustafa <mgords@gmail.com>
2015-09-02 22:03:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,92 +1,56 @@
 /*
-mg52 - Mus 
+mg52 - Mus
 */
 var port = process.env.PORT || 5000;
 var express = require('express');
-var app = express();
-var server = app.listen(port);
+var expressSession = require('express-session');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );      
-app.use(bodyParser.urlencoded({    
-  extended: true
-})); 
+var app = express();
+
+var cons = require('consolidate');
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+
+app.use(cookieParser());
+app.use(expressSession({secret:'MusSecret'}));
+app.use(bodyParser());
 
 var mongoose = require('mongoose');
-var uristring = process.env.MONGOLAB_URI;
+var uristring = process.env.MONGOLAB_URI || 'mongodb://localhost/musnote_a';
 mongoose.connect(uristring);
 var UserSchema = new mongoose.Schema({
 	username: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
-	email: {type: String, required: true, unique: true},
-  	admin: Boolean,
+	email: {type: String, required: true},
   	notes: String
 });
 var User = mongoose.model('User', UserSchema);
-//module.exports = User;
-
-var expressSession = require('express-session');
-app.use(expressSession({secret:'musSecret'}));
-
-app.set('view engine', 'jade');
 
 var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 
-var users = [];
-var usercount = 0;
-var deleteduser = '';
-var sendusers = new String;
-
 app.get('/', function (req, res) {
-	if(req.session.user){
-	if(!req.session.admin)
+	if(req.session.user)
 		res.redirect('/login');
 	else
-		res.redirect('/admin');
-	}
-	else 
 		res.render('index');
 });
 app.get('/login', function (req, res) {
 	if(req.session.user)
-		res.render('login', { loginmessage: req.session.user + " !", 
-		username: req.session.user, 
+		res.render('login', { loginmessage: req.session.user + " !",
+		username: req.session.user,
 		password: req.session.password,
 		email: req.session.email,
-		admin: req.session.admin,
 		notes: req.session.notes});
-	else 
-		res.redirect('/');
-});
-app.get('/admin', function (req, res) {
-	if(req.session.user){
-	if(deleteduser == '')
-		res.render('admin', { loginmessage: req.session.user + " !", 
-		username: req.session.user, 
-		password: req.session.password,
-		email: req.session.email,
-		admin: req.session.admin, 
-		totalusers: sendusers
-		});
 	else
-		res.render('admin', { loginmessage: req.session.user + " !", 
-		username: req.session.user, 
-		password: req.session.password,
-		email: req.session.email,
-		admin: req.session.admin,
-		totalusers: sendusers,
-		deleteduser: deleteduser + " deleted!"
-		});
-		deleteduser = '';
-		}
-	else 
 		res.redirect('/');
 });
+
 app.post('/login', function (req, res) {
   	delete req.session.user;
   	delete req.session.password;
 	delete req.session.email;
-  	delete req.session.admin;
 	delete req.session.notes;
   	var post = req.body;
   	User.findOne({ 'username': post.user }, function(err, user) {
@@ -96,28 +60,13 @@ app.post('/login', function (req, res) {
 		req.session.user = user.username;
 		req.session.password = user.password;
 		req.session.email = user.email;
-		req.session.admin = user.admin;
 		req.session.notes = user.notes;
 		console.log(req.session.user + " logged in.");
-		if(req.session.admin == true){
-			findall();
-			/*sendgrid.send({
-			to:       'x@x.com',
-  			from:     'mus@mus.com',
-  			subject:  'Hello World',
-  			text:     'Selam baba' + req.session.user
-			}, function(err, json) {
-  				if (err) { return console.error(err); }
-  				console.log(json);
-			});*/
-			res.redirect('/admin');
-		}
-		else
-			res.redirect('/login');
+		res.redirect('/login');
 	}
-	else res.redirect('badUserLogIn');
+	else res.redirect('/badUserLogIn');
   }
-  else res.redirect('badUserLogIn');
+  else res.redirect('/badUserLogIn');
 });
 });
 app.post('/addNote', function (req,res) {
@@ -138,8 +87,8 @@ app.post('/addNote', function (req,res) {
 			username: req.session.user,
 			password: req.session.password,
 			email: req.session.email,
-			admin: req.session.admin,
-			notes: req.session.notes
+			notes: req.session.notes,
+			saved: "Note Saved!"
 		});
 	}
 	else
@@ -157,14 +106,12 @@ app.post('/addUser', function (req, res) {
   	req.session.user = post.adduser;
   	req.session.password = post.addpassword;
 	req.session.email = post.addemail;
-  	req.session.admin = false;
 	req.session.notes = "";
   	var new_user = new User({
   	username: req.session.user,
   	password: req.session.password,
 	email: req.session.email,
-  	admin: req.session.admin,
-	notes: req.session.notes	
+	notes: req.session.notes
 });
 	new_user.save(function (err, new_user) {
             if (err) {
@@ -178,10 +125,62 @@ app.post('/addUser', function (req, res) {
         });
 });
 app.get('/badUserSignUp', function (req, res) {
-	res.render('index', { signupmessage: "Bad Username or Password"});
+	res.render('index', { signupmessage: "This username is using by someone else!"});
 });
 app.get('/badUserLogIn', function (req, res) {
 	res.render('index', { loginmessage: "Bad Username or Password"});
+});
+app.get('/changePassword', function (req, res) {
+	res.render('changePassword');
+});
+app.post('/changePassword', function (req, res){
+	if(req.session.user){
+		res.redirect('/changePassword');
+	}
+	else
+		res.redirect('/');
+});
+app.post('/passwordChanged', function(req, res){
+	var post = req.body;
+	User.findOne({ 'username': req.session.user }, function(err, user) {
+		if (err) throw err;
+	 	user.password = post.password;
+		user.save(function(err) {
+			if (err) throw err;
+			console.log("New Password Saved.");
+		});
+	 });
+	res.render('login', {
+		loginmessage: req.session.user + " !",
+		username: req.session.user,
+		password: req.session.password,
+		email: req.session.email,
+		notes: req.session.notes,
+		saved: "Your password has been changed sucessfully!"
+	});
+});
+app.post('/sendNote', function(req, res){
+	if(req.session.user){
+		sendgrid.send({
+			to:       req.session.email,
+			from:     'no-reply@musnote.com',
+			subject:  'Your Note',
+			text:     req.session.notes
+			}, function(err, json) {
+				if (err) throw err;
+				console.log(json);
+		});
+		res.render('login', {
+			loginmessage: req.session.user + " !",
+			username: req.session.user,
+			password: req.session.password,
+			email: req.session.email,
+			notes: req.session.notes,
+			sendNoteMessage: "Your note has been sent to your email!"
+		});
+	}
+	else
+		res.redirect('/');
 });
 app.post('/delete', function (req, res) {
   var deleted_user = req.session.user;
@@ -192,53 +191,17 @@ app.post('/delete', function (req, res) {
   	delete req.session.user;
   	delete req.session.password;
 	delete req.session.email;
- 	delete req.session.admin;
  	delete req.session.notes;
 	res.redirect('/');
 });
-app.post('/deletebyname', function (req, res) {
-  var post = req.body;
-  
-  if(post.username != 'mustafa')
-  User.findOneAndRemove({ username: post.username }, function(err, numberRemoved) {
-	if (err) throw err;
-	if(numberRemoved != null){
-	deleteduser = post.username;
-	console.log(deleteduser + ' deleted!');
-	}
-	findall();
-    res.redirect('/admin');
-  });
-  else
-	res.redirect('/admin');
-  
-});
+
 app.post('/logout', function (req, res) {
   	delete req.session.user;
   	delete req.session.password;
 	delete req.session.email;
-  	delete req.session.admin;
 	delete req.session.notes;
   	res.redirect('/');
-});  
-function findall(){
-	users = [];
-	User.count({}, function( err, count){
-		console.log( "Number of users:", count );
-		usercount = count;
-	});
-	
-	User.find({}, function(err, user) {
-		var a = 0;
-		for(i = 0;i<usercount;i++){
-			users[a] = user[i].username;
-			a++;
-			users[a] = user[i].password;
-			a++;
-		}
-		sendusers = users.toString();
-	});
-}
-findall();
-console.log("Working!");
+});
 
+var server = app.listen(port);
+console.log("Working!");
